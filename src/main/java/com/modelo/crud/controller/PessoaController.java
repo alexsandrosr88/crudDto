3a7e8ff0387e60471ec2af610753ff96b5ee160f
alexsandrosr88 <alexsandrosr@msn.com>
2023-02-11 03:11:27 -0300

    Feat: Novo Endpoint("/pessoa/{id}") Delete - deletar pessoa por id.

--- src/main/java/com/modelo/crud/controller/PessoaController.java
+++ src/main/java/com/modelo/crud/controller/PessoaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,9 @@ public class PessoaController {
 		return service.editarPessoa(dto, id);
 	}
 	
-	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping(value = "/{id}")
+	public void deletarPessoa(@PathVariable Long id) {
+		service.deletarPessoa(id);
+	}
 }
--- src/main/java/com/modelo/crud/service/PessoaService.java
+++ src/main/java/com/modelo/crud/service/PessoaService.java
@@ -1,7 +1,6 @@
 package com.modelo.crud.service;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,8 +47,9 @@ public class PessoaService {
 		pessoa = new Pessoa(dto, id);
 		return new PessoaDTO(repository.save(pessoa));
 	}
-	
-	
-	
-	
+
+	@Transactional(readOnly = false)
+	public void deletarPessoa(Long id) {
+		repository.deleteById(id);
+	}
 }
